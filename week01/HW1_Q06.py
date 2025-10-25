3b905c612ca16eb27dabd9666486822b63bd0d5a
parsapartovi1 <pparsapartovi13@gmail.com>
2025-10-25 15:13:47 +0330

    question 6

---
+++ week01/HW1_Q06.py
@@ -0,0 +1,18 @@
+
+#HOME WORK 6 :
+
+#MUSIC
+name = "music name"
+singer_name = "singer name"
+duration =int("00:00")
+volume = int("0 <= volume <= max")
+print(name)
+
+#YOUTUBE
+title = "title name"
+channel = "channel name"
+subscribe_button = True or False
+like_button = True or False
+upload_time = int("upload_time")
+amount_of_like = int("amount_of_like")
+duration = int("00:00")

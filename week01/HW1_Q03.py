024216f30f7d3d34cfebd78d47835008162a3470
parsapartovi1 <pparsapartovi13@gmail.com>
2025-10-25 15:07:56 +0330

    question 3

---
+++ week01/HW1_Q03.py
@@ -0,0 +1,11 @@
+#HOME WORK 3 :
+
+temperature = int(input("Enter the temp(c): "))
+if temperature <= 10 :
+    print("the weather is cold")
+elif 10 < temperature < 25 :
+    print("the weather is normal")
+elif temperature >= 25 :
+    print("its hot")
+
+

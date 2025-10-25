ba647316171155f1579571da778a33d9dd677668
parsapartovi1 <pparsapartovi13@gmail.com>
2025-10-25 15:11:09 +0330

    question 5

---
+++ week01/HW1_Q05.py
@@ -0,0 +1,16 @@
+#HOME WORK 5 :
+
+factorial = 1
+for x in range(1, 7):
+    factorial *= x
+    print("factorial is equalled to", factorial)
+
+
+x = 1
+factorial = 1
+while x <= 6:
+    factorial = factorial * x
+    x = x + 1
+    print(f"factorial is equalled to", factorial)
+
+

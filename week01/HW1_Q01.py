d734928bc537665ca23d3d694158837ab9adc97a
parsapartovi1 <pparsapartovi13@gmail.com>
2025-10-25 15:03:37 +0330

    question 1

---
+++ week01/HW1_Q01.py
@@ -0,0 +1,13 @@
+#PROJECT ONE WEEK ONE M.S
+
+#HOME WORK 1 :
+
+# first_name: str =input( "enter the 1st name : " )
+# second_name: str = input(" enter the 2nd name : " )
+#
+# first_name = first_name.replace("a ", " . ")
+# second_name = second_name.replace("i ", " . ")
+# both = first_name + " " + second_name
+# print(both)
+
+
